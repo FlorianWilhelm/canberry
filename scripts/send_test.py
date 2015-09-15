3c015d3706ee10bef5ea8fccd6f1ace69b811f0e
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2015-09-15 14:53:21 +0200

    Few improvements

--- scripts/send_test.py
+++ scripts/send_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function, absolute_import, division
@@ -13,12 +14,20 @@ from canberry.can_utils import make_sdo
 
 _logger = logging.getLogger(__name__)
 
+
 def main(args):
     bus = can.interface.Bus()
     msg = make_sdo(recipient=0x42, index=0x32)
-    _l
-    bus.send(msg)
+    _logger.info("Sending message...")
+    if bus.send(msg) < 0:
+        _logger.info("Message not sent!")
+    else:
+        _logger.info("Message sent!")
+    _logger.info("Waiting for message...")
+    answer = bus.recv()
+    _logger.info("Message received:\n{}".format(answer))
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     main(sys.argv[1:])
--- tests/test_can_utils.py
+++ tests/test_can_utils.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function, absolute_import, division
-
-__author__ = 'Florian Wilhelm'
-__copyright__ = 'Florian Wilhelm'
-
-
 from canberry.can_utils import *
 
 
