227e94c19621d725c9d5361b643fdc570fa92f91
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2015-09-15 16:23:04 +0200

    Wire to flask

--- canberry/views.py
+++ canberry/views.py
@@ -3,6 +3,13 @@ from __future__ import absolute_import, division, print_function
 from . import app
 
 
+from .logic import get_speed
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+
+@app.route('/sensors/speed')
+def speed():
+    return get_speed()
