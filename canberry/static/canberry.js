022f38010b8bc382c0f3faa18c17369e7c186af2
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2015-09-27 00:20:23 +0200

    Output of dummy sensor works kind of

--- canberry/static/canberry.js
+++ canberry/static/canberry.js
@@ -1,39 +1,41 @@
 var sensorData = (function () {
     var data = []; // private
-    var pub = {};// public object - returned at end of module
+    var pub = {}; // public object - returned at end of module
 
     pub.addData = function(value) {
         data.push(value);
     };
 
     pub.clearData = function() {
-        data = []
+        data = [];
     };
 
     pub.getData = function() {
         return data;
-    }
+    };
+
+    pub.plotData = function() {
+        var points = [];
+		for (var i = 0; i < data.length; ++i) {
+			points.push([i, data[i]])
+		}
+        $.plot("#placeholder", [points], {
+            xaxis: {
+			    show: false
+			}
+		});
+    };
 
     return pub; // expose externally
 }());
 
+function updateGraph() {
+    $.get('./sensors/dummy', function(data, status) {
+        data = JSON.parse(data);
+        sensorData.addData(data.parameter)});
+    sensorData.plotData();
+    }
 
-function plotSensorData() {
-
-		var d1 = [];
-		for (var i = 0; i < 14; i += 0.5) {
-			d1.push([i, Math.sin(i)]);
-		}
-
-		var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-
-		// A null signifies separate line segments
-
-		var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-
-		$.plot("#placeholder", [ d1, d2, d3 ]);
-
-		// Add the Flot version string to the footer
-
-		$("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
-	}
+$( document ).ready(function() {
+    setInterval(updateGraph, 1000);
+});
