5e7c1ffe37fdc96d7e26146230a252ee3f2ff5db
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2015-09-28 21:21:00 +0200

    Fix importing bug

--- canberry/contrib/can/__init__.py
+++ canberry/contrib/can/__init__.py
@@ -16,9 +16,9 @@ rc = dict(channel=0)
 class CanError(IOError):
     pass
 
-from can.CAN import BufferedReader, Listener, Printer, CSVWriter, SqliteWriter, set_logging_level
-from can.message import Message
-from can.bus import BusABC
-from can.notifier import Notifier
-from can.broadcastmanager import send_periodic, CyclicSendTaskABC, MultiRateCyclicSendTaskABC
-from can.interfaces import interface
+from .CAN import BufferedReader, Listener, Printer, CSVWriter, SqliteWriter, set_logging_level
+from .message import Message
+from .bus import BusABC
+from .notifier import Notifier
+from .broadcastmanager import send_periodic, CyclicSendTaskABC, MultiRateCyclicSendTaskABC
+from .interfaces import interface
